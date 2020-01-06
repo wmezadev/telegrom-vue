1846bda2448d038335db57e733cc0bdd2a8b3079
William Eduardo Meza <williameduardomeza@gmail.com>
2020-01-05 21:07:10 -0500

    Se corrige error en rutas

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import User from './views/Home.vue';
+import User from './views/User.vue';
 import Home from './views/Home.vue';
 import Chat from './views/Chat.vue';
 import Messages from './views/Messages.vue';
@@ -14,6 +14,11 @@ export default new Router({
     {
       path: '/',
       name: 'home',
+      component: Home,
+    },
+    {
+      path: '/',
+      name: 'user',
       component: User,
     },
     {
